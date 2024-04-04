ffad34cac646e3eadd5a385256d7659a72cea079
eloizamaria09 <166056590+eloizamaria09@users.noreply.github.com>
2024-04-04 15:11:23 -0300

    atividade do facebook
    atividade do facebook: conta da nina dobrev

---
+++ funcoes.js
@@ -0,0 +1,20 @@
+nome = document.getElementById("nomedoditocujo");
+nome.innerHTML = user.name;
+
+foto = document.getElementById("fotododitocujo");
+foto.src = user.photo;
+
+idade = document.getElementById('idadedoditocujo');
+idade.innerHTML = user.idade;
+
+pai = document.getElementById("paidoditocujo");
+pai.innerHTML = user.pai;
+
+mae = document.getElementById("maedoditocujo");
+mae.innerHTML = user.mae;
+
+sexo = document.getElementById('sexododitocujo');
+sexo.innerHTML = user.sexo;
+
+sobremim = document.getElementById('sobremim')
+sobremim.innerHTML = user.sobremim;
